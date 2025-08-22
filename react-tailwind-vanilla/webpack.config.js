df74075cde94eaa3d57e4152c6e5c2180c1fcaa7
GurmukhSKharod <gurmukhkharod@hotmail.com>
2025-08-21 18:37:18 -0700

    connect react to render.

--- react-tailwind-vanilla/webpack.config.js
+++ react-tailwind-vanilla/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,8 +14,9 @@ module.exports = {
     static: './dist',
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
+    new HtmlWebpackPlugin({ template: 'public/index.html' }), // or your template
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_API_BASE': JSON.stringify(process.env.REACT_APP_API_BASE || '')
     }),
   ],
   module: {
